97370c0fb77fe10782db27a0be6b3ef75acb21aa
hayoungishere <48154977+hayoungishere@users.noreply.github.com>
2019-06-25 14:39:35 +0900

    My first java project

---
+++ project1/src/project1/OperatorTwoClass.java
@@ -0,0 +1,27 @@
+package project1;
+
+public class OperatorTwoClass {
+	public static void main(String[] args) {
+		
+		int intValue=10;
+		int intValue2=5;
+		
+		System.out.println(intValue+intValue2);
+		System.out.println(intValue-intValue2);
+		System.out.println(intValue*intValue2);
+		System.out.println(intValue/intValue2);
+		System.out.println(intValue%intValue2);
+		
+		
+		//이항 연산자 문자열
+		String s1 = "Hello"; // 문자열 참조형 변수
+		String s2 = "World";
+		
+		System.out.println(s1+s2);
+		
+		//비교 연산자 boolean
+		System.out.println(3!=3);
+		
+		
+	}
+}
